e58f2c07328657d5fb5864030a69cb7393039b4b
kimura koji <k.kimura.3343@gmail.com>
2019-09-18 14:02:25 +0900

    Implement basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  # app_controllerにincludeですべてのコントローラで使用可能になる。
+  include SessionsHelper
+
   def hello
     render html: "hello, world"
   end
